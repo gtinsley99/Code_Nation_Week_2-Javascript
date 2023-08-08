5c08c14c279991b0b35c5912520634221414724e
George Tinsley <george8s@live.co.uk>
2023-08-08 09:35:27 +0100

    cleaner activity 3 maths calculation

--- Day1/activity3.js
+++ Day1/activity3.js
@@ -2,7 +2,7 @@ const birthDate = new Date(99, 4, 5);
 
 const dateToday = Date.now()
 
-const daysAlive = (dateToday - birthDate)/(3600000 * 24)
+const daysAlive = (dateToday - birthDate)/(1000 * 60 * 60 * 24)
 
 console.log(`I have been alive for ${daysAlive.toFixed(0)} days.`);
 
