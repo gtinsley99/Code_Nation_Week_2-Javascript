68ae36bfd148f1ca39deb6390d3e468a2e4bef08
George Tinsley <george8s@live.co.uk>
2023-08-10 15:07:38 +0100

    completed activity 3 on Day 4

--- Day4/activity3.js
+++ Day4/activity3.js
@@ -12,28 +12,37 @@ const coffeeShop = {
         "croissant", 0.70
     ],
     drinksOrdered(){
+        let sum = 0;
         for (i = 0; i < this.yourOrder.length; i++){
             for (j = 0; j < this.drinksWithPrices.length; j++){
                 if (this.yourOrder[i] === this.drinksWithPrices[j]){
-                    let sum = 0;
                     sum += this.drinksWithPrices[j+1];
-                    return `Your drinks order is ${this.drinksWithPrices[j]} - £${this.drinksWithPrices[(j+1)].toFixed(2)} for a total of £${sum.toFixed(2)}.`;
+                    console.log(`Your drinks order is ${this.drinksWithPrices[j]} - £${this.drinksWithPrices[(j+1)].toFixed(2)} for a total of £${sum.toFixed(2)}.`);
                 }
             }
         }
     },       
     foodOrdered(){
-        return `Your food order is`
+        let sum = 0;
+        for (i = 0; i < this.yourOrder.length; i++){
+            for (j = 0; j < this.foodWithPrices.length; j++){
+                if (this.yourOrder[i] === this.foodWithPrices[j]){
+                    sum += this.foodWithPrices[j+1];
+                    console.log(`Your food order is ${this.foodWithPrices[j]} - £${this.foodWithPrices[(j+1)].toFixed(2)} for a total of £${sum.toFixed(2)}.`);
+                }
+            }
+        }
     }
 }
 
-coffeeShop.yourOrder = ["coffee", "sandwich", "tea"];
+coffeeShop.yourOrder = ["coffee", "sandwich", "tea", "croissant"];
 
-console.log(coffeeShop.drinksOrdered())
+// coffeeShop.drinksOrdered();
+
+coffeeShop.foodOrdered();
 
 
 
-// NOT DONE YET
 
 
 
